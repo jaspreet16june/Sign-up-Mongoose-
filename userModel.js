374d2717e92b66c5a80b67b3d03eaa81e600453b
jaspreet kaur <jaspreetkaur16june@gmail.com>
2021-09-21 15:31:49 +0530

    Sign Up

---
+++ userModel.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const {db_link}= require('../secret.js');
+const validator = require('email-validator');
+
+mongoose.connect(db_link).then(function(){
+    console.log('db connected');
+})
+.catch(function(err){
+    console.log(err)
+})
+
+const userSchema  = new mongoose.Schema({
+
+    name:{
+        type: String,
+        required: true,
+    },
+    age:{
+        type:Number,
+    },
+    email:{
+        type:String,
+        required:true,
+        validate:function(){
+            return validator.validate(this.email);
+        }
+    },
+    password:{
+        type:String,
+        required:true,
+        min:8,
+    },
+    confirmPassword:{
+        type:String,
+        required: true,
+        validate:function(){
+            return this.password == this.confirmPassword;
+        },
+    }
+})
+
+
+const userModel = mongoose.model('userModel',userSchema);
+
+(async function createUser(){
+     
+   let user={
+            name : 'Harsimran singh',
+            age:21,
+            email:'harsimranSingh@gmail.com',
+            password:'123abcde',
+            confirmPassword:'123abcde',
+    }
+
+    let userObj = await userModel.create(user);
+    console.log(userObj);
+})();
